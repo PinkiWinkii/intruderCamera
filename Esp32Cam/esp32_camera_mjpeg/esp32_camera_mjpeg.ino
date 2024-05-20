fedd19ddf4372dea851df51aa8b3b837836ef30c
PinkiWinki <asier.arguinchona@ikasle.aeg.eus>
2024-05-20 13:17:41 +0200

    BOTH WORKING AS INTENDED

--- Esp32Cam/esp32_camera_mjpeg/esp32_camera_mjpeg.ino
+++ Esp32Cam/esp32_camera_mjpeg/esp32_camera_mjpeg.ino
@@ -1,93 +1,69 @@
-#include "src/OV2640.h"
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
 #include <WiFi.h>
-#include <WiFiMulti.h>
-#include <WiFiClient.h>
+#include "src/OV2640.h"
 
-// Select camera model
-//#define CAMERA_MODEL_WROVER_KIT
+
+const char* ssid = "GONBIDATUAK";
+const char* password = "FAC3200BBC";
+const char* mqtt_server = "7b97ddb1a02b45308522e93ed8f9bcd1.s1.eu.hivemq.cloud";
+const char* mqtt_topic = "control-led";
+const int mqtt_port = 8883;
+const char* mqtt_username = "ledpatata2";
+const char* mqtt_password = "ABCD1234e";
+
+static const char* root_ca PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
+
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
+// Selecciona el modelo de cámara
 #define CAMERA_MODEL_ESP_EYE
+//#define CAMERA_MODEL_WROVER_KIT
 //#define CAMERA_MODEL_M5STACK_PSRAM
 //#define CAMERA_MODEL_M5STACK_WIDE
 //#define CAMERA_MODEL_AI_THINKER
 
 #include "camera_pins.h"
 
-#define SSID1 "POCO F3"
-#define PWD1 "llabakawifi"
-
 OV2640 cam;
 
-WiFiMulti wifiMulti;
+void setup() {
+  
 
-WiFiServer server(80);
 
-const char HEADER[] = "HTTP/1.1 200 OK\r\n" \
-                      "Access-Control-Allow-Origin: *\r\n" \
-                      "Content-Type: multipart/x-mixed-replace; boundary=123456789000000000000987654321\r\n";
-const char BOUNDARY[] = "\r\n--123456789000000000000987654321\r\n";
-const char CTNTTYPE[] = "Content-Type: image/jpeg\r\nContent-Length: ";
-const int hdrLen = strlen(HEADER);
-const int bdrLen = strlen(BOUNDARY);
-const int cntLen = strlen(CTNTTYPE);
-
-void handle_jpg_stream(WiFiClient client)
-{
-  char buf[32];
-  int s;
-
-  client.write(HEADER, hdrLen);
-  client.write(BOUNDARY, bdrLen);
-
-  while (true)
-  {
-    if (!client.connected()) break;
-    cam.run();
-    s = cam.getSize();
-    client.write(CTNTTYPE, cntLen);
-    sprintf( buf, "%d\r\n\r\n", s );
-    client.write(buf, strlen(buf));
-    client.write((char *)cam.getfb(), s);
-    client.write(BOUNDARY, bdrLen);
-  }
-}
-
-const char JHEADER[] = "HTTP/1.1 200 OK\r\n" \
-                       "Content-disposition: inline; filename=capture.jpg\r\n" \
-                       "Content-type: image/jpeg\r\n\r\n";
-const int jhdLen = strlen(JHEADER);
-
-void handle_jpg(WiFiClient client)
-{
-  cam.run();
-  if (!client.connected()) return;
-
-  client.write(JHEADER, jhdLen);
-  client.write((char *)cam.getfb(), cam.getSize());
-}
-
-void handleNotFound()
-{
-  WiFiClient client = server.available();
-
-  String message = "Server is running!\n\n";
-  message += "URI: ";
-  message += "/"; // URI not available with WiFiServer, so just show root
-  message += "\nMethod: ";
-  message += "GET"; // Method not available, assuming it's always GET
-  message += "\nArguments: ";
-  message += "0"; // Arguments not available, assuming 0
-  message += "\n";
-  client.println(HEADER);
-  client.println("Content-Type: text/plain");
-  client.println("Connection: close");
-  client.println();
-  client.println(message);
-}
-
-void setup()
-{
-  Serial.begin(115200);
-  //while (!Serial);            //wait for serial connection.
 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -111,63 +87,108 @@ void setup()
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
 
-  // Frame parameters
-  //  config.frame_size = FRAMESIZE_UXGA;
   config.frame_size = FRAMESIZE_QVGA;
   config.jpeg_quality = 12;
   config.fb_count = 2;
 
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
-
   cam.init(config);
 
-  IPAddress ip;
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID1, PWD1);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(F("."));
-  }
-  ip = WiFi.localIP();
-  Serial.println(F("WiFi connected"));
-  Serial.println("");
-  Serial.println(ip);
-  Serial.print("Stream Link: http://");
-  Serial.print(ip);
-  Serial.println("/mjpeg/1");
-
-  server.begin();
-}
-
-void loop()
-{
-  WiFiClient client = server.available();
-  if (client)
-  {
-    if (client.connected())
-    {
-      if (client.available())
-      {
-        String req = client.readStringUntil('\r');
-        if (req.indexOf("/mjpeg/1") != -1)
-        {
-          handle_jpg_stream(client);
-        }
-        else if (req.indexOf("/jpg") != -1)
-        {
-          handle_jpg(client);
-        }
-        else
-        {
-          handleNotFound();
-        }
-        client.stop();
-      }
+  Serial.begin(9600);
+  client.setClient(espClient);
+  setup_wifi();
+  espClient.setCACert(root_ca);
+  client.setServer(mqtt_server, mqtt_port);
+  
+  while (!client.connected()) {
+    Serial.print("Intentando conexión a MQTT...");
+    if (client.connect("ESP32Client", mqtt_username, mqtt_password)) {
+      Serial.println("conectado");
+      client.subscribe(mqtt_topic);
+      client.setCallback(callback);
+    } else {
+      Serial.print("falló, rc=");
+      Serial.print(client.state());
+      Serial.println(" intentando de nuevo en 5 segundos");
+      delay(5000);
     }
   }
+  //client.connect("ESP32Client", mqtt_username, mqtt_password);
+
+
+
+
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Conectando a ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi conectado");
+  Serial.println("Dirección IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Mensaje recibido [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+}
+
+void capture_photo() {
+  // Capturar foto con la cámara
+  cam.run();
+  const uint8_t *imageData = cam.getfb();
+  size_t imageSize = cam.getSize();
+  
+  // Aquí puedes enviar la foto a través de MQTT, guardarla en el sistema de archivos, etc.
+  // Por ahora, solo imprimimos el tamaño de la imagen y los primeros 10 bytes como ejemplo
+  Serial.print("Tamaño de la imagen: ");
+  Serial.println(imageSize);
+  Serial.println("Primeros 10 bytes de la imagen:");
+  for (int i = 0; i < 10; i++) {
+    Serial.print(imageData[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Intentando conexión a MQTT...");
+    if (client.connect("ESP32Client2", mqtt_username, mqtt_password)) {
+      Serial.println("conectado");
+      if (client.subscribe(mqtt_topic)) {
+        Serial.print("Subscrito al topic ");
+        Serial.println(mqtt_topic);
+      } else {
+        Serial.println("Error al suscribirse al topic");
+      }
+      client.setCallback(callback);
+    } else {
+      Serial.print("falló, rc=");
+      Serial.print(client.state());
+      Serial.println(" intentando de nuevo en 1 segundos");
+      delay(1000);
+    }
+  }
+}
+
+void loop() {
+  if(!client.connected())
+  {
+    reconnect();
+  }
+
+  client.loop();
 }
