729d5f51900eeeb6da942642a3a72cdc8019a28f
PinkiWinki <asier.arguinchona@ikasle.aeg.eus>
2024-05-21 12:26:48 +0200

    Camera working as intended with new buffer change

--- Esp32CamPaid/Esp32CamPaid.ino
+++ Esp32CamPaid/Esp32CamPaid.ino
@@ -6,57 +6,32 @@
 
 const char* ssid = "GONBIDATUAK";
 const char* password = "FAC3200BBC";
-const char* mqtt_server = "7b97ddb1a02b45308522e93ed8f9bcd1.s1.eu.hivemq.cloud";
+const char* mqtt_server = "52.206.254.73";
 const char* mqtt_topic = "control-led";
 const char* mqtt_topic2 = "photo";
 const int mqtt_port = 8883;
 const char* mqtt_username = "ledpatata2";
 const char* mqtt_password = "ABCD1234e";
 
-static const char* root_ca PROGMEM = R"EOF(
------BEGIN CERTIFICATE-----
-MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
-TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
-cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
-WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
-ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
-MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
-h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
-0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
-A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
-T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
-B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
-B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
-KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
-OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
-jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
-qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
-rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
-HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
-hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
-ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
-3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
-NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
-ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
-TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
-jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
-oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
-4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
-mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
-emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
------END CERTIFICATE-----
-)EOF";
-
 WiFiClientSecure espClient;
 PubSubClient client(espClient);
 
 // Selecciona el modelo de cámara
 #define CAMERA_MODEL_ESP_EYE
+//#define CAMERA_MODEL_WROVER_KIT
+//#define CAMERA_MODEL_M5STACK_PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE
+//#define CAMERA_MODEL_AI_THINKER
+
 #include "camera_pins.h"
 
 OV2640 cam;
 
 void setup() {
+  
+
+
+
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -79,27 +54,30 @@ void setup() {
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
 
-  config.frame_size = FRAMESIZE_QVGA;
+  config.frame_size = FRAMESIZE_QQVGA;
   config.jpeg_quality = 10;
-  config.fb_count = 2;
+  config.fb_count = 1;
 
   cam.init(config);
 
   Serial.begin(9600);
   client.setClient(espClient);
   setup_wifi();
-  espClient.setCACert(root_ca);
-  client.setServer(mqtt_server, mqtt_port);
+  //espClient.setCACert(root_ca);
+  client.setServer(mqtt_server, 1883);
+  
+  //client.connect("ESP32Client", mqtt_username, mqtt_password);
   client.setCallback(callback);
-  client.setBufferSize(20000);
+
+
+
 }
 
 void setup_wifi() {
-  delay(10);
+// Conecta al WiFi
   Serial.println();
   Serial.print("Conectando a ");
   Serial.println(ssid);
-
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -114,7 +92,9 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  if (strcmp(topic, mqtt_topic2) == 0) {
+
+  if (strcmp(topic, mqtt_topic2) == 0) 
+  {
     return;
   }
 
@@ -124,20 +104,26 @@ void callback(char* topic, byte* payload, unsigned int length) {
   capture_photo();
 }
 
-void capture_photo() {
-  Serial.print("Memoria libre antes de capturar: ");
-  Serial.println(ESP.getFreeHeap());
-
+void capture_photo2() {
+  Serial.println("Capturando foto...");
   camera_fb_t * fb = esp_camera_fb_get();
   if (!fb) {
     Serial.println("Error al capturar la imagen");
     return;
   }
+  
+  // Convertir los datos de la imagen a base64
+  String imageBase64 = base64::encode(fb->buf, fb->len);
+  Serial.println(imageBase64.c_str());
+  // Publicar la imagen en base64
+  if (!client.connected()) {
+    Serial.println("Error: cliente MQTT no conectado");
+    esp_camera_fb_return(fb);
+    return;
+  }
 
-  Serial.print("Memoria libre después de capturar: ");
-  Serial.println(ESP.getFreeHeap());
-
-  if (!client.publish(mqtt_topic2, fb->buf, fb->len)) {
+  // Publicar los datos de la imagen en base64
+  if (!client.publish(mqtt_topic2, (const uint8_t*)imageBase64.c_str(), imageBase64.length(), false)) {
     Serial.println("Error al publicar la imagen");
   } else {
     Serial.println("Imagen publicada correctamente");
@@ -145,15 +131,48 @@ void capture_photo() {
 
   // Liberar la memoria utilizada por el framebuffer
   esp_camera_fb_return(fb);
-
-  Serial.print("Memoria libre después de liberar framebuffer: ");
-  Serial.println(ESP.getFreeHeap());
 }
+
+void capture_photo() {
+  camera_fb_t * fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Error al capturar la imagen");
+    return;
+  }
+  
+  // Convertir los datos de la imagen a hexadecimal
+  String imageHex = "";
+  for (size_t i = 0; i < fb->len; i++) {
+    char hex[3];
+    sprintf(hex, "%02X", fb->buf[i]);
+    imageHex += hex;
+  }
+  
+  // Publicar la imagen en formato hexadecimal
+  if (!client.connected()) {
+    Serial.println("Error: cliente MQTT no conectado");
+    esp_camera_fb_return(fb);
+    return;
+  }
+
+  Serial.println(imageHex.c_str());
+
+  // Publicar los datos de la imagen en formato hexadecimal
+  if (!client.publish(mqtt_topic2, imageHex.c_str())) {
+    Serial.println("Error al publicar la imagen");
+  } else {
+    Serial.println("Imagen publicada correctamente");
+  }
+
+  // Liberar la memoria utilizada por el framebuffer
+  esp_camera_fb_return(fb);
+}
+
 
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Intentando conexión a MQTT...");
-    if (client.connect("ESP32Client2", mqtt_username, mqtt_password)) {
+    if (client.connect("ESP32Client2")) {
       Serial.println("conectado");
       if (client.subscribe(mqtt_topic)) {
         Serial.print("Subscrito al topic ");
@@ -181,7 +200,8 @@ void reconnect() {
 }
 
 void loop() {
-  if (!client.connected()) {
+  if(!client.connected())
+  {
     reconnect();
   }
 
